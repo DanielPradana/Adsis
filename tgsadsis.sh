be72b2517bd417912afe922a60e99b11eb7f3bf3
DanielPradana <dpradana@ti.ukdw.ac.id>
2017-11-19 21:26:48 +0700

    monitorServer
    Monitor Server

--- tgsadsis.sh
+++ tgsadsis.sh
@@ -19,3 +19,17 @@ do
 done
 
 }
+
+monitorSystem () {
+printf "Memory\t\tDisk\t\tCPU\n"
+SECONDS=$(date +'%S')
+end=$((SECONDS+3600))
+while [ $SECONDS -lt $end ];
+do
+	m=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+	d=$(df -h | awk 'NR==2{printf "%s\t\t", $5}')
+	c=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+	echo "$m$d$c"
+sleep 1
+done
+}
